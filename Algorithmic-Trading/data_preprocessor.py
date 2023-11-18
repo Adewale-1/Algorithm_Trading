cfa6215237bba711811ac0e6f7435a9ed8941877
Adewale-Adenle <56052969+Adewale-1@users.noreply.github.com>
2023-11-18 00:40:11 -0500

    Update py

--- Algorithmic-Trading/data_preprocessor.py
+++ Algorithmic-Trading/data_preprocessor.py
@@ -10,63 +10,66 @@ def load_data(filename):
 
 # Clean data
 def clean_data(df):
-    # Drop any rows with missing values
-    df = df.dropna()
-    return df
+    # Remove NaNs which might affect indicators calculation
+    df_cleaned = df.dropna()
+    return df_cleaned
 
 # Add technical indicators as features
 def add_technical_indicators(df):
-    # Add moving averages
-    df['SMA_50'] = talib.SMA(df['Close'].values, timeperiod=50)
-    df['SMA_200'] = talib.SMA(df['Close'].values, timeperiod=200)
+    # Add Simple Moving Averages
+    df['SMA_50'] = talib.SMA(df['Close'], timeperiod=50)
+    df['SMA_200'] = talib.SMA(df['Close'], timeperiod=200)
 
-    # Add RSI
-    df['RSI_14'] = talib.RSI(df['Close'].values, timeperiod=14)
+    # Add Relative Strength Index
+    df['RSI_14'] = talib.RSI(df['Close'], timeperiod=14)
 
-    # Add MACD
-    macd, macdsignal, _ = talib.MACD(df['Close'].values, fastperiod=12, slowperiod=26, signalperiod=9)
+    # Add Moving Average Convergence Divergence
+    macd, macdsignal, _ = talib.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
     df['MACD'] = macd
-    df['MACD_signal'] = macdsignal
+    df['MACD_Signal'] = macdsignal
 
     # Add Bollinger Bands
-    upperband, middleband, lowerband = talib.BBANDS(df['Close'].values, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
-    df['BB_upper'] = upperband
-    df['BB_middle'] = middleband
-    df['BB_lower'] = lowerband
+    upperband, middleband, lowerband = talib.BBANDS(df['Close'], timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+    df['Upper_BB'] = upperband
+    df['Middle_BB'] = middleband
+    df['Lower_BB'] = lowerband
 
+    # Drop rows with NaN values introduced by indicator windows
+    df = df.dropna()
     return df
 
-# Normalize data
-def normalize_data(df):
+# Scale features with a StandardScaler
+def scale_features(X_train, X_test):
     scaler = StandardScaler()
-    df_scaled = pd.DataFrame(scaler.fit_transform(df), columns=df.columns, index=df.index)
-    return df_scaled
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
-# Main pre-processing function
+    return X_train_scaled, X_test_scaled
+
+# Main preprocessing function
 def preprocess_data(filename):
     df = load_data(filename)
     df_cleaned = clean_data(df)
-    df_features = add_technical_indicators(df_cleaned)
-    df_final = normalize_data(df_features)
-    return df_final
-
-# Split data into training and testing sets
-def split_data(df, test_size=0.2):
-    feature_columns = df.columns.difference(['Close'])
-    X = df[feature_columns]
-    y = df['Close']
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=False)
-    return X_train, X_test, y_train, y_test
+    df_indicators = add_technical_indicators(df_cleaned)
+    
+    # Extract the Close price to be the target variable
+    target = df_indicators.pop('Close')
+    features = df_indicators
+    
+    # Normalize the feature data
+    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, shuffle=False)
+    X_train_scaled, X_test_scaled = scale_features(X_train, X_test)
+    
+    return X_train_scaled, y_train, X_test_scaled, y_test
 
 if __name__ == "__main__":
-    filename = 'EURUSD=X >>> 1d >>> 2020-01-01 >>> 2023-11-17.csv'  # Mutable with the filename
-    df_preprocessed = preprocess_data(filename)
-    X_train, X_test, y_train, y_test = split_data(df_preprocessed)
+    filename = 'EURUSD=X >>> 1d >>> 2020-01-01 >>> 2023-11-17.csv' # replace with your CSV file
     
-    # Optionally, save the preprocessed data to new CSV files
-    df_preprocessed.to_csv('preprocessed_data.csv')
-    X_train.to_csv('train_features.csv')
-    y_train.to_csv('train_targets.csv')
-    X_test.to_csv('test_features.csv')
-    y_test.to_csv('test_targets.csv')
+    # Run the preprocessing
+    X_train_scaled, y_train, X_test_scaled, y_test = preprocess_data(filename)
+    
+    # Save the preprocessed data to CSV files for later use
+    pd.DataFrame(X_train_scaled).to_csv('scaled_train_features.csv', index=False)
+    y_train.to_csv('train_targets.csv', index=False)
+    pd.DataFrame(X_test_scaled).to_csv('scaled_test_features.csv', index=False)
+    y_test.to_csv('test_targets.csv', index=False)
---
+++ Algorithmic-Trading/lstm_predictor.py
@@ -0,0 +1,91 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+from keras.callbacks import EarlyStopping
+from sklearn.metrics import mean_squared_error
+import tensorflow as tf
+
+#  Load datasets
+def load_data(train_features_file, train_targets_file, test_features_file, test_targets_file):
+    X_train = pd.read_csv(train_features_file)
+    y_train = pd.read_csv(train_targets_file)
+    X_test = pd.read_csv(test_features_file)
+    y_test = pd.read_csv(test_targets_file)
+    return X_train, y_train.values.ravel(), X_test, y_test.values.ravel()
+
+# Reshape features for LSTM
+def reshape_features_for_lstm(X):
+    return np.reshape(X.values, (X.shape[0], X.shape[1], 1))
+
+# Build LSTM model
+def build_lstm_model(input_shape):
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=input_shape))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))
+
+    # Use the TensorFlow optimizer with gradient clipping
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, clipvalue=1.0)
+    model.compile(optimizer=optimizer, loss='mean_squared_error')
+    return model
+
+
+# Train LSTM model
+def train_lstm_model(X_train, y_train, batch_size, epochs):
+    X_train_reshaped = reshape_features_for_lstm(X_train)
+    model = build_lstm_model((X_train_reshaped.shape[1], 1))
+
+    # Early stopping to prevent overfitting
+    early_stopping = EarlyStopping(
+        monitor='val_loss', mode='min', verbose=1, patience=10)
+
+    model.fit(X_train_reshaped, y_train, epochs=epochs, batch_size=batch_size,
+              verbose=1, validation_split=0.2, callbacks=[early_stopping])
+    return model
+
+# Evaluate LSTM model
+
+
+def evaluate_lstm_model(model, X_test, y_test):
+    X_test_reshaped = reshape_features_for_lstm(X_test)
+    y_pred = model.predict(X_test_reshaped)
+
+    mse = mean_squared_error(y_test, y_pred)
+    print(f"The Mean Squared Error on the test set is: {mse}")
+
+    # Plotting predictions vs actual values for visual inspection
+    plt.plot(y_test, color='red', label='Actual EURUSD Price')
+    plt.plot(y_pred, color='blue', label='Predicted EURUSD Price')
+    plt.title('EURUSD Price Prediction')
+    plt.xlabel('Time')
+    plt.ylabel('EURUSD Price')
+    plt.legend()
+    plt.show()
+
+    return mse
+
+
+if __name__ == "__main__":
+    train_features_file = 'scaled_train_features.csv'
+    train_targets_file = 'train_targets.csv'
+    test_features_file = 'scaled_test_features.csv'
+    test_targets_file = 'test_targets.csv'
+
+    X_train, y_train, X_test, y_test = load_data(
+        'scaled_train_features.csv', 
+        'train_targets.csv', 
+        'scaled_test_features.csv', 
+        'test_targets.csv'
+    )
+    
+    print(f'X_train shape: {X_train.shape}')
+    print(f'y_train shape: {y_train.shape}')
+    
+    model = train_lstm_model(X_train, y_train, batch_size=64, epochs=50)
+
+    mse = evaluate_lstm_model(model, X_test, y_test)
